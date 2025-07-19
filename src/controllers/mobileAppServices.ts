899d6aaeb98b3db54829a798c96a2b024d6cf7c9
Sankar <settisankar95@gmail.com>
2025-07-19 19:47:21 +0530

    update

--- src/controllers/mobileAppServices.ts
+++ src/controllers/mobileAppServices.ts
@@ -23,6 +23,12 @@ export default class MobileAppServices {
     this.router.get("/bussinessprofiles", this.getbussinessProfiles.bind(this));
     this.router.get("/bussinessprofilesBySearch", this.getbussinessProfilesBySearch.bind(this));
     this.router.get("/bussinessprofilesByid", this.getbussinessProfilesByid.bind(this));
+    this.router.get("/adds", this.getAdds.bind(this));
+    this.router.get("/addByid", this.getAddByid.bind(this));
+    this.router.get("/locations", this.getLocations.bind(this));
+    this.router.get("/locationByid", this.getLocationByid.bind(this));
+
+
     this.router.post("/users", this.createUser.bind(this));
     this.router.post("/login", this.Login.bind(this));
     this.router.put("/resetpass", this.resetPass.bind(this));
@@ -42,10 +48,10 @@ export default class MobileAppServices {
 
         // Check for duplicate employee (by EMAIL and MOBILE_NUMBER)
         const checkDup = `SELECT COUNT(EMAIL) as count FROM SECURITY_LOGIN WHERE EMAIL = ?`;
-        const dupResult = await executeDbQuery(checkDup, [input.EMAIL], false, apiName, port, connection);
-        if (Number(dupResult[0]?.count) > 0) {
+        const dupdata = await executeDbQuery(checkDup, [input.EMAIL], false, apiName, port, connection);
+        if (Number(dupdata[0]?.count) > 0) {
           await connection.rollback();
-          res.json({ status: 2, result: "User already exists." });
+          res.json({ status: 2, data: "User already exists." });
           return;
         }
 
@@ -56,13 +62,13 @@ export default class MobileAppServices {
 
         const insertQuery = `INSERT INTO SECURITY_LOGIN (CITY_ID, USER_ID, USERNAME, EMAIL, PASSWORD, LOCATION, STATUS, CREATED_BY) VALUES (?, ?, ?, ?, ?, ?, ?, ?)`;
         const params = ['101', LoginId, input.NAME, input.EMAIL, input.PASSWORD, input.LOCATION, 'A', userId ];
-        const result = await executeDbQuery(insertQuery, params, false, apiName, port, connection);
+        const data = await executeDbQuery(insertQuery, params, false, apiName, port, connection);
         await connection.commit();
-        res.json({ status: 0, result: { message: "User created", LoginId: LoginId, affectedRows: result.affectedRows, },
+        res.json({ status: 0, data: { message: "User created", LoginId: LoginId, affectedRows: data.affectedRows, },
         });
       } catch (err: any) {
         if (connection) await connection.rollback();
-        res.json({ status: 1, result: err.toString() });
+        res.json({ status: 1, data: err.toString() });
       } finally {
         if (connection) connection.release();
       }
@@ -76,14 +82,14 @@ export default class MobileAppServices {
           let query = `SELECT USER_ID ID, USERNAME USER_NAME, EMAIL MAIL_ID FROM SECURITY_LOGIN WHERE EMAIL=? AND PASSWORD=? AND STATUS='A'`;
           const params = [input.email, input.password];
     
-          const result = await executeDbQuery(query, params, false, apiName);
-          if (result.length >= 1) {
-            Response.json({ status: 0, result: { message: "Login Success", results: result } });
+          const data = await executeDbQuery(query, params, false, apiName);
+          if (data.length >= 1) {
+            Response.json({ status: 0, data: { message: "Login Success", datas: data } });
           } else {
-            Response.json({ status: 2, result: { message: "user not found" } });
+            Response.json({ status: 2, data: { message: "user not found" } });
           }
         } catch (err: any) {
-          Response.json({ status: 1, result: err.toString() });
+          Response.json({ status: 1, data: err.toString() });
         }
       }
 
@@ -97,34 +103,34 @@ export default class MobileAppServices {
       await connection.beginTransaction();
 
       const chekdup = `SELECT COUNT(USER_ID) as count FROM SECURITY_LOGIN WHERE USER_ID = ?`;
-          const dupResult = await executeDbQuery(chekdup, [userId], false, apiName, port, connection);
-          if (Number(dupResult[0]?.count) == 0) {
+          const dupdata = await executeDbQuery(chekdup, [userId], false, apiName, port, connection);
+          if (Number(dupdata[0]?.count) == 0) {
               await connection.rollback();
-              res.json({ status: 2, result: "Invalid User." });
+              res.json({ status: 2, data: "Invalid User." });
               return;
           }
 
     const updateQuery = "UPDATE SECURITY_LOGIN SET PASSWORD = ? WHERE USER_ID = ?";
     const params = [ input.NEW_PASSWORD, userId ];
     try {
-      const result = await executeDbQuery(updateQuery, params, true, apiName, port);
-      const results = {message: "Password updated"}
-      res.json({ status: 0, result:results });
+      const data = await executeDbQuery(updateQuery, params, true, apiName, port);
+      const datas = {message: "Password updated"}
+      res.json({ status: 0, data:datas });
     } catch (err: any) {
-      res.json({ status: 1, result: err.toString() });
+      res.json({ status: 1, data: err.toString() });
     }
   }
 
     async getFeed(req: Request, res: Response) {
       const apiName = "App/newsfeed";
       const port = req.socket.localPort!;
-      const query = `SELECT FEED_ID ID, FEED_HEAD TITLE, FEED_MATTER DESCRIPTION, IMAGE_URL FEED_IMAGE FROM NEWS_FEED WHERE STATUS='A' ORDER BY CREATED_ON DESC`;
+      const query = `SELECT FEED_ID id, FEED_HEAD tittle, FEED_MATTER description, IMAGE_URL imageUrl FROM NEWS_FEED WHERE STATUS='A' ORDER BY CREATED_ON DESC`;
   
       try {
         const rows = await executeDbQuery(query, [], false, apiName, port);
-        res.json({ status: 0, result: rows });
+        res.json({ status: 0, data: rows });
       } catch (err: any) {
-        res.json({ status: 1, result: err.toString() });
+        res.json({ status: 1, data: err.toString() });
       }
     }
 
@@ -135,22 +141,22 @@ export default class MobileAppServices {
   
       try {
         const rows = await executeDbQuery(query, [], false, apiName, port);
-        res.json({ status: 0, result: rows });
+        res.json({ status: 0, data: rows });
       } catch (err: any) {
-        res.json({ status: 1, result: err.toString() });
+        res.json({ status: 1, data: err.toString() });
       }
     }
 
     async getServices(req: Request, res: Response) {
       const apiName = "App/services";
       const port = req.socket.localPort!;
-      const query = `SELECT ID, NAME SERVICE_NAME, IMAGE_URL SERVICE_IMAGE FROM SERVICES WHERE STATUS='A' ORDER BY NAME`;
+      const query = `SELECT ID id, NAME serviceName , IMAGE_URL imageUrl FROM SERVICES WHERE STATUS='A' ORDER BY NAME`;
   
       try {
         const rows = await executeDbQuery(query, [], false, apiName, port);
-        res.json({ status: 0, result: rows });
+        res.json({ status: 0, data: rows });
       } catch (err: any) {
-        res.json({ status: 1, result: err.toString() });
+        res.json({ status: 1, data: err.toString() });
       }
     }
 
@@ -158,26 +164,26 @@ export default class MobileAppServices {
       const apiName = "App/SubServices";
       const port = req.socket.localPort!;
       const ServiceId = req.query.id || "";
-      const query = `SELECT SUB_SERVICE_ID ID, NAME SUB_SERVICE_NAME, IMAGE_URL SUB_SERVICE_IMAGE FROM SUB_SERVICES WHERE STATUS='A' AND SERVICE_ID = ? ORDER BY NAME`;
+      const query = `SELECT SUB_SERVICE_ID id, NAME subServiceName, IMAGE_URL imageUrl FROM SUB_SERVICES WHERE STATUS='A' AND SERVICE_ID = ? ORDER BY NAME`;
   
       try {
         const rows = await executeDbQuery(query, [ServiceId], false, apiName, port);
-        res.json({ status: 0, result: rows });
+        res.json({ status: 0, data: rows });
       } catch (err: any) {
-        res.json({ status: 1, result: err.toString() });
+        res.json({ status: 1, data: err.toString() });
       }
     }
 
     async getJobs(req: Request, res: Response) {
       const apiName = "App/Jobs";
       const port = req.socket.localPort!;
-      const query = `SELECT JOB_ID ID, JOB_TITLE TITLE, EXPERIENCE, JOB_TYPE TYPE, IMAGE_URL JOB_IMAGE FROM JOBS WHERE STATUS='A' ORDER BY CREATED_ON DESC`;
+      const query = `SELECT JOB_ID id, JOB_TITLE title, EXPERIENCE experience, JOB_TYPE type, IMAGE_URL imageUrl FROM JOBS WHERE STATUS='A' ORDER BY CREATED_ON DESC`;
   
       try {
         const rows = await executeDbQuery(query, [], false, apiName, port);
-        res.json({ status: 0, result: rows });
+        res.json({ status: 0, data: rows });
       } catch (err: any) {
-        res.json({ status: 1, result: err.toString() });
+        res.json({ status: 1, data: err.toString() });
       }
     }
 
@@ -185,26 +191,26 @@ export default class MobileAppServices {
       const apiName = "App/getJobsByid";
       const port = req.socket.localPort!;
       const JobId = req.query.id || "";
-      const query = `SELECT JOB_ID ID, JOB_TITLE TITLE, PACKAGE SALARY_PACKAGE, EXPERIENCE, JOB_TYPE TYPE, DESCRIPTION JOB_DESCRIPTION, IMAGE_URL JOB_IMAGE FROM JOBS WHERE STATUS='A' AND JOB_ID=? ORDER BY CREATED_ON DESC`;
+      const query = `SELECT JOB_ID id, JOB_TITLE title, PACKAGE package, EXPERIENCE experience, JOB_TYPE jobType, DESCRIPTION description, IMAGE_URL imageUrl FROM JOBS WHERE STATUS='A' AND JOB_ID=?`;
   
       try {
         const rows = await executeDbQuery(query, [JobId], false, apiName, port);
-        res.json({ status: 0, result: rows });
+        res.json({ status: 0, data: rows });
       } catch (err: any) {
-        res.json({ status: 1, result: err.toString() });
+        res.json({ status: 1, data: err.toString() });
       }
     }
 
     async getbussinessProfiles(req: Request, res: Response) {
       const apiName = "App/BussinessProfile";
       const port = req.socket.localPort!;
-      const query = `SELECT BUSINESS_ID ID, BUSINESS_NAME NAME, ADDRESS BUSSINESS_ADDRESS, IMAGE_URL1 BUSSINESS_IMAGE FROM BUSSINESS_PROFILE WHERE STATUS='A' ORDER BY BUSINESS_NAME`;
+      const query = `SELECT BUSINESS_ID id, BUSINESS_NAME name, ADDRESS address, IMAGE_URL1 imageUrl FROM BUSSINESS_PROFILE WHERE STATUS='A' ORDER BY BUSINESS_NAME`;
   
       try {
         const rows = await executeDbQuery(query, [], false, apiName, port);
-        res.json({ status: 0, result: rows });
+        res.json({ status: 0, data: rows });
       } catch (err: any) {
-        res.json({ status: 1, result: err.toString() });
+        res.json({ status: 1, data: err.toString() });
       }
     }
 
@@ -212,13 +218,13 @@ export default class MobileAppServices {
       const apiName = "App/BussinessProfileById";
       const port = req.socket.localPort!;
       const BusinessId = req.query.id || "";
-      const query = `SELECT BUSINESS_ID ID, BUSINESS_NAME NAME, BUSINESS_TYPE TYPE, ADDRESS BUSSINESS_ADDRESS, IMAGE_URL1 BUSSINESS_IMAGE, WEEKDAY_TIMINGS TIMINGS, DEFAULT_CONTACT CONTACT FROM BUSSINESS_PROFILE WHERE STATUS='A' AND BUSINESS_ID=? ORDER BY BUSINESS_NAME `;
+      const query = `SELECT BUSINESS_ID id, BUSINESS_NAME name, BUSINESS_TYPE type, ADDRESS address, IMAGE_URL1 imageUrl, WEEKDAY_TIMINGS timings, DEFAULT_CONTACT mobile FROM BUSSINESS_PROFILE WHERE STATUS='A' AND BUSINESS_ID=? `;
   
       try {
         const rows = await executeDbQuery(query, [BusinessId], false, apiName, port);
-        res.json({ status: 0, result: rows });
+        res.json({ status: 0, data: rows });
       } catch (err: any) {
-        res.json({ status: 1, result: err.toString() });
+        res.json({ status: 1, data: err.toString() });
       }
     }
 
@@ -226,7 +232,7 @@ export default class MobileAppServices {
       const apiName = "App/BussinessProfileBySearch";
       const port = req.socket.localPort!;
       const SearchBy = req.query.q ? String(req.query.q) : "";
-      let query = `SELECT BUSINESS_ID ID, BUSINESS_NAME NAME, BUSINESS_TYPE TYPE, ADDRESS BUSSINESS_ADDRESS, IMAGE_URL1 BUSSINESS_IMAGE FROM BUSSINESS_PROFILE WHERE STATUS='A' LIMIT 15`;
+      let query = `SELECT BUSINESS_ID id, BUSINESS_NAME name, BUSINESS_TYPE type, ADDRESS address, IMAGE_URL1 imageUrl FROM BUSSINESS_PROFILE WHERE STATUS='A' LIMIT 15`;
   
         if (SearchBy) {
           query += ` AND BUSINESS_NAME LIKE '%${SearchBy}%' ORDER BY BUSINESS_NAME`;
@@ -234,9 +240,62 @@ export default class MobileAppServices {
 
       try {
         const rows = await executeDbQuery(query, [SearchBy], false, apiName, port);
-        res.json({ status: 0, result: rows });
+        res.json({ status: 0, data: rows });
       } catch (err: any) {
-        res.json({ status: 1, result: err.toString() });
+        res.json({ status: 1, data: err.toString() });
+      }
+    }
+
+    async getAdds(req: Request, res: Response) {
+      const apiName = "App/Adds";
+      const port = req.socket.localPort!;
+      const query = `SELECT ID id, DESCRIPTION description, IMAGE_URL imageUrl FROM ADVERTISEMENTS WHERE STATUS='A' ORDER by CREATED_ON DESC`;
+  
+      try {
+        const rows = await executeDbQuery(query, [], false, apiName, port);
+        res.json({ status: 0, data: rows });
+      } catch (err: any) {
+        res.json({ status: 1, data: err.toString() });
+      }
+    }
+
+    async getAddByid(req: Request, res: Response) {
+      const apiName = "App/getAddByid";
+      const port = req.socket.localPort!;
+      const AddId = req.query.id || "";
+      const query = `SELECT ID id, DESCRIPTION description, IMAGE_URL imageUrl FROM ADVERTISEMENTS WHERE STATUS='A' and id=?`;
+  
+      try {
+        const rows = await executeDbQuery(query, [AddId], false, apiName, port);
+        res.json({ status: 0, data: rows });
+      } catch (err: any) {
+        res.json({ status: 1, data: err.toString() });
+      }
+    }
+    async getLocations(req: Request, res: Response) {
+      const apiName = "App/Locations";
+      const port = req.socket.localPort!;
+      const query = `SELECT LOCAT_ID id, LOCAT_NAME name, BEST_TIME bestTime, ATTRACTIONS attractions, ACCESSIBILITY accessibility, OVERVIEW overview, IMAGE_URL1 imageUrl1, IMAGE_URL2 imageUrl2, IMAGE_URL3 imageUrl3, IMAGE_URL4 imageUrl4, IMAGE_URL5 imageUrl5 FROM NEAR_LOCATION where STATUS='A' ORDER BY LOCAT_NAME ASC`;
+  
+      try {
+        const rows = await executeDbQuery(query, [], false, apiName, port);
+        res.json({ status: 0, data: rows });
+      } catch (err: any) {
+        res.json({ status: 1, data: err.toString() });
+      }
+    }
+
+    async getLocationByid(req: Request, res: Response) {
+      const apiName = "App/getAddByid";
+      const port = req.socket.localPort!;
+      const LocateId = req.query.id || "";
+      const query = `SELECT LOCAT_ID id, LOCAT_NAME name, BEST_TIME bestTime, ATTRACTIONS attractions, ACCESSIBILITY accessibility, OVERVIEW overview, IMAGE_URL1 imageUrl1, IMAGE_URL2 imageUrl2, IMAGE_URL3 imageUrl3, IMAGE_URL4 imageUrl4, IMAGE_URL5 imageUrl5 FROM NEAR_LOCATION where STATUS='A' and LOCAT_ID=?`;
+  
+      try {
+        const rows = await executeDbQuery(query, [LocateId], false, apiName, port);
+        res.json({ status: 0, data: rows });
+      } catch (err: any) {
+        res.json({ status: 1, data: err.toString() });
       }
     }
 }
