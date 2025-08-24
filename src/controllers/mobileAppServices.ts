04e730d39a33ba9f8856a42225ee21c8834613c9
Sankar <settisankar95@gmail.com>
2025-08-24 09:46:21 +0530

    update

--- src/controllers/mobileAppServices.ts
+++ src/controllers/mobileAppServices.ts
@@ -263,7 +263,7 @@ export default class MobileAppServices {
   async getbussinessBySIids(req: Request, res: Response) {
     const apiName = "App/BussinessBySIids";
     const port = req.socket.localPort!;
-    const SubServiceID: any = req.query.id || "";
+    const SubServiceID: any = req.query.subServiceid || "";
     const ServiceId = req.params.id || "";
     let perams = [];
     let query = `SELECT BUSINESS_ID id, BUSINESS_NAME name, BUSINESS_TYPE type, ADDRESS address, IMAGE_URL1 imageUrl, WEEKDAY_TIMINGS timings, DEFAULT_CONTACT mobile FROM BUSSINESS_PROFILE WHERE STATUS='A'  AND SERVICE_ID=? `;
